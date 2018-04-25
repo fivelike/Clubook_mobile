d217eedaebb0f91bfad5db7cb7afc7ce2ce6f78a
fivelike <xfivelike@hotmail.com>
2018-04-25 23:12:32 +0800

    添加card及toolbar滑动布局

--- src/pages/circle/circle.ts
+++ src/pages/circle/circle.ts
@@ -1,25 +1,53 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the CirclePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
+import { Component, NgZone, ViewChild } from '@angular/core';
+import { NavController, Content, Tabs } from 'ionic-angular';
 @Component({
   selector: 'page-circle',
   templateUrl: 'circle.html',
 })
 export class CirclePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild(Content) content: Content;
+  public display: boolean = true;
+
+  circle: string = "recommend";
+
+  public circles: any;
+  constructor(public navCtrl: NavController,
+    public ngzone: NgZone) {
+
+    this.circles = [
+      { "name": "圈子1" },
+      { "name": "圈子2" },
+      { "name": "圈子3" },
+      { "name": "圈子4" },
+      { "name": "圈子5" },
+      { "name": "圈子6" },
+      { "name": "圈子7" },
+      { "name": "圈子8" }
+    ];
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CirclePage');
+
+
+  //滑动隐藏
+  scrollHandler(e) {
+    this.ngzone.run(() => {
+      if (this.content.directionY == "down") {
+        // console.log("down");
+        this.display = false;
+        this.hiddenTabs(true);
+        this.content.resize();
+      } else {
+        this.display = true;
+        this.hiddenTabs(false);
+        this.content.resize();
+      }
+    })
+  }
+  //隐藏tabs
+  hiddenTabs(p: boolean) {
+    let t: Tabs = this.navCtrl.parent;
+    t.setTabbarHidden(p);
   }
 
 }
--- src/pages/club/club.ts
+++ src/pages/club/club.ts
@@ -1,25 +1,54 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, NgZone, ViewChild } from '@angular/core';
+import { NavController, Content, Tabs } from 'ionic-angular';
 
-/**
- * Generated class for the ClubPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-club',
   templateUrl: 'club.html',
 })
 export class ClubPage {
+  @ViewChild(Content) content: Content;
+  public display: boolean = true;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  club: string = "recommend";
+
+  public clubs:any;
+  constructor(public navCtrl: NavController,
+    public ngzone: NgZone) {
+
+      this.clubs = [
+    { "name": "社团1" },
+    { "name": "社团2" },
+    { "name": "社团3" },
+    { "name": "社团4" },
+    { "name": "社团5" },
+    { "name": "社团6" },
+    { "name": "社团7" },
+    { "name": "社团8" }
+  ];
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ClubPage');
+
+
+  //滑动隐藏
+  scrollHandler(e) {
+    this.ngzone.run(() => {
+      if (this.content.directionY == "down") {
+        // console.log("down");
+        this.display = false;
+        this.hiddenTabs(true);
+        this.content.resize();
+      } else {
+        this.display = true;
+        this.hiddenTabs(false);
+        this.content.resize();
+      }
+    })
   }
+  //隐藏tabs
+  hiddenTabs(p: boolean) {
+    let t: Tabs = this.navCtrl.parent;
+    t.setTabbarHidden(p);
+  }
+
 
 }
